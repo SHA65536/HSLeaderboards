142ce7f44f5a41418d36d574b026fc7f781de951
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-06-04 22:13:27 +0300

    added config loading

--- config.go
+++ config.go
@@ -1,4 +1,26 @@
 package hsleaderboards
 
+import (
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
+	Interval int
+	DBPath   string
+}
+
+func LoadConfig() *Config {
+	godotenv.Load()
+	var interval = 600
+	val, err := strconv.Atoi(os.Getenv("INTERVAL"))
+	if err != nil {
+		interval = val
+	}
+	return &Config{
+		DBPath:   os.Getenv("DB_PATH"),
+		Interval: interval,
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hsleaderboards
 
 go 1.18
+
+require github.com/joho/godotenv v1.4.0
